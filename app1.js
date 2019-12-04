82951422910e09110a8b23b7e408371484f5532d
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-12-04 12:48:19 +0200

    not finshed yet

---
+++ app1.js
@@ -0,0 +1,5 @@
+'use strict';
+function myFunction() {
+    document.getElementById("form").submit();
+  }
+  myFunction();
---
+++ app2.js
@@ -0,0 +1,17 @@
+'use strict';
+var numbers = [];
+var greates;
+function Result(
+    namber
+    ){
+this.namber = number;
+numbers.push(this);
+}
+function greaterThan(){
+    for (let i = 0; i < numbers.length; i++) {
+       if (numbers[i] > number[i-1]) {
+            greates = numbers[i];
+       }
+        
+    }
+}
